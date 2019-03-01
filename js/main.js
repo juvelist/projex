84704b51573284da1853905d976ce94ba4cda231
juvelist <39278128+juvelist@users.noreply.github.com>
2019-03-01 16:46:38 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ var modalBlockView = (function() {
     var welcomeBlock = $('#welcome-block'),
         welcomeModal = $('#welcome-modal'),
         welcomeClose = $('#welcome-close'),
-        blockShowingspeed = 700,
-        blockShowingTimeIn = 3000,
+        blockShowingSpeed = 700,
+        blockShowingTimeIn = 10000,
         blockShowingTimeOut = 7000,
         blockRemovingTime = 1000;
 
     setTimeout(function(){
-        welcomeBlock.fadeIn(blockShowingspeed);
-        welcomeModal.animate({top: '0px'}, blockShowingspeed);
+        welcomeBlock.fadeIn(blockShowingSpeed);
+        welcomeModal.stop().animate({top: '0px'}, blockShowingSpeed);
 
         setTimeout(function(){
-            welcomeBlock.fadeOut(blockShowingspeed);
-            welcomeModal.animate({top: '-2500px'}, blockShowingspeed);
+            welcomeBlock.fadeOut(blockShowingSpeed);
+            welcomeModal.stop().animate({top: '-2500px'}, blockShowingSpeed);
 
             setTimeout(function(){
                 welcomeBlock.remove();
@@ -23,7 +23,11 @@ var modalBlockView = (function() {
     }, blockShowingTimeIn);
 
     welcomeClose.click(function () {
-        welcomeBlock.fadeOut(blockShowingspeed);
-        welcomeModal.animate({top: '-2500px'}, blockShowingspeed);
+        welcomeBlock.fadeOut(blockShowingSpeed);
+        welcomeModal.stop().animate({top: '-2500px'}, blockShowingSpeed);
     });
 }());
+
+
+// .stop().fadeToggle()
+// .hide().remove()
